7927e698ede764286b4b14e8a96a99301dbdf968
kalorak <kalotai92@gmail.com>
2025-02-02 17:46:39 +0100

    Added calculator service

--- app.py
+++ app.py
@@ -24,6 +24,12 @@ def home():
 def about():
     return render_template("about.html")
 
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))  # Default to 10000
+    app.run(host="0.0.0.0", port=port)
+
 # Projects route
 @app.route("/projects")
 def projects():
